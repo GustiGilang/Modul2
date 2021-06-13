08bf57dc527563d183b316f2298c9be37038e369
GustiGilang <gustigilang98@gmail.com>
2021-06-14 01:12:37 +0800

    Initial commit

---
+++ src/model/MahasiswaBeraksi.java
@@ -0,0 +1,35 @@
+package model;
+
+import com.sun.corba.se.spi.orbutil.fsm.FSMImpl;
+import com.sun.org.apache.xerces.internal.xs.XSImplementation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(polaTanggal);
+
+        //instance - pembuatan object
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.npm = "19630321";
+        mahasiswa.nama = "GustiGilangHaifaFadillah";
+        mahasiswa.tanggallahir = sdf.parse("24-02-2001");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggallahir);
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.npm = "19630123";
+        mahasiswa2.nama = "GustiGhithaHaifaFadillah";
+        mahasiswa2.tanggallahir = new Date();
+
+        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggallahir);
+    }
+}
