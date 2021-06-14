ab7cc7ad1413995ad5d6739f6dfde9d2d0e52bc4
GustiGilang <gustigilang98@gmail.com>
2021-06-15 00:23:15 +0800

    format tanggal dan try catch

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
         String polaTanggal = "dd-MM-yyy";
         SimpleDateFormat sdf = new SimpleDateFormat(polaTanggal);
@@ -17,20 +17,30 @@ public class MahasiswaBeraksi {
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19630321";
         mahasiswa.nama = "GustiGilangHaifaFadillah";
-        mahasiswa.tanggallahir = sdf.parse("24-02-2001");
+        try {
+            mahasiswa.tanggallahir = sdf.parse("24-02-2001");
 
-        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
-        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
-        System.out.println("Tanggal Lahir \t : " + sdf.format(mahasiswa.tanggallahir));
+            System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+            System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+            System.out.println("Tanggal Lahir \t : " + sdf.format(mahasiswa.tanggallahir));
+        } catch (ParseException e) {
+            System.err.println("Format tanggal tidak sesuai");
+        }
+
 
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19630123";
         mahasiswa2.nama = "GustiGhithaHaifaFadillah";
-        mahasiswa2.tanggallahir =sdf.parse("13-05-1999");
+        try {
+            mahasiswa2.tanggallahir =sdf.parse("13-05-1999");
+
+            System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+            System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+            System.out.println("Tanggal Lahir \t : " + sdf.format(mahasiswa2.tanggallahir));
+        } catch (ParseException e) {
+            System.err.println("Format tanggal tidak sesuai");
+        }
 
 
-        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
-        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
-        System.out.println("Tanggal Lahir \t : " + sdf.format(mahasiswa.tanggallahir));
     }
 }
